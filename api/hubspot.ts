b53bfdd05bace9efee4f26eff22e7adb14937b7a
Katarina Du Couteau <zain.fake02@gmail.com>
2022-04-28 12:39:31 +0000

    feat: update lifecyclestage default value

--- api/hubspot.ts
+++ api/hubspot.ts
@@ -50,7 +50,7 @@ export default async (data: GetPeopleQuery): Promise<HubspotResult> => {
         expa_referrer: el?.referral_type!,
         hs_lead_status: "NOT_CONTACTED",
         program_interested_in: program_interested_in!,
-        lifecyclestage: "Intersted",
+        lifecyclestage: "intersted",
         expa_created_date,
         hubspot_owner_id,
         phone,
