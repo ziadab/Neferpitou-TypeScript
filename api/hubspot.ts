a853da89cc4fd7e44a1fe22c27b8c574a3e0fe88
Ziad Abouelfarah <zain.fake02@gmail.com>
2022-05-18 14:14:32 +0100

    feat: randomize the ep

--- api/hubspot.ts
+++ api/hubspot.ts
@@ -1,20 +1,20 @@
-import dayjs from "dayjs"
-import { parsePhoneNumber } from "libphonenumber-js"
-import { GetPeopleQuery } from "."
-import hubspotClient from "../clients/HubSpotClient"
-import product_to_manager from "../utils/product_to_manager"
-import utc from "dayjs/plugin/utc"
+import dayjs from "dayjs";
+import { parsePhoneNumber } from "libphonenumber-js";
+import { GetPeopleQuery } from ".";
+import hubspotClient from "../clients/HubSpotClient";
+import product_to_manager from "../utils/product_to_manager";
+import utc from "dayjs/plugin/utc";
 
-dayjs.extend(utc)
+dayjs.extend(utc);
 
 interface HubspotResult {
-  error: Array<string>
-  success: Array<string>
+  error: Array<string>;
+  success: Array<string>;
 }
 
 export default async (data: GetPeopleQuery): Promise<HubspotResult> => {
-  const error: string[] = []
-  const success: string[] = []
+  const error: string[] = [];
+  const success: string[] = [];
   data.people?.data?.map(async (el, index) => {
     const phone =
       el?.contact_detail?.phone! == null
@@ -22,24 +22,24 @@ export default async (data: GetPeopleQuery): Promise<HubspotResult> => {
         : parsePhoneNumber(
             el?.contact_detail?.phone!,
             "MA"
-          )?.formatInternational()!
+          )?.formatInternational()!;
     const program_interested_in =
       el?.person_profile?.selected_programmes?.length == 0
         ? "0"
-        : el?.person_profile?.selected_programmes![0]?.toString()
+        : el?.person_profile?.selected_programmes![0]?.toString();
     const hubspot_owner_id =
       program_interested_in == "0"
-        ? index % 2 == 0
+        ? Math.random() < 0.5
           ? "273765855"
           : "296102633"
-        : (product_to_manager as any)[program_interested_in!]
+        : (product_to_manager as any)[program_interested_in!];
     const expa_created_date = dayjs(el?.created_at)
       .utc()
       .second(0)
       .minute(0)
       .hour(0)
       .valueOf()
-      .toString()
+      .toString();
 
     const contact = {
       properties: {
@@ -55,28 +55,28 @@ export default async (data: GetPeopleQuery): Promise<HubspotResult> => {
         hubspot_owner_id,
         phone,
       },
-    }
+    };
     // console.log(el?.id)
     await hubspotClient.crm.contacts.basicApi
       .create(contact)
       .then((res) => {
         // console.log("success: ", el?.id)
-        success.push(el?.id!)
+        success.push(el?.id!);
       })
       .catch((err) => {
         // console.log("fail: ", el?.id)
-        console.log(err)
+        console.log(err);
         // console.log(el?.id!)
-        error.push(el?.id!)
+        error.push(el?.id!);
       })
       .finally(() => {
-        return { error, success }
-      })
-  })
-  await new Promise((f) => setTimeout(f, 3500))
+        return { error, success };
+      });
+  });
+  await new Promise((f) => setTimeout(f, 3500));
 
-  return { error, success }
-}
+  return { error, success };
+};
 
 // 1427997766000
 // 1646582855000
