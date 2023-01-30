1dc729cfd7208dad4cd42c68530eaa2164791a80
Ziad Abouelfarah <zain.fake02@gmail.com>
2023-01-30 11:41:06 +0100

    f

--- app.ts
+++ app.ts
@@ -1,24 +1,27 @@
-import dotenv from "dotenv"
-import express from "express"
-import cors from "cors"
-import githubHandler from "./routes/githubWebhook"
-import hubspotHundler from "./routes/hubspot"
-import telegramHandler from "./routes/telegram"
-import cronHandler from "./routes/cron"
-import typeformHandler from "./routes/typeform"
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import githubHandler from "./routes/githubWebhook";
+import hubspotHundler from "./routes/hubspot";
+import telegramHandler from "./routes/telegram";
+import cronHandler from "./routes/cron";
+import typeformHandler from "./routes/typeform";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use("/github", githubHandler)
-app.use("/hubspot", hubspotHundler)
-app.use("/telegram", telegramHandler)
-app.use("/cron", cronHandler)
-app.use("/typeform", typeformHandler)
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/github", githubHandler);
+app.use("/hubspot", hubspotHundler);
+app.use("/telegram", telegramHandler);
+app.use("/cron", cronHandler);
+app.use("/typeform", typeformHandler);
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Listening on ${port}...`)
-})
+  console.log(`Listening on ${port}...`);
+});
