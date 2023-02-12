786c0eee856d4e0170c2b8b669adf1544f2dd3b5
Ziad Abouelfarah <zain.fake02@gmail.com>
2023-02-12 20:49:39 +0100

    telegram updates

--- routes/telegram.ts
+++ routes/telegram.ts
@@ -1,5 +1,28 @@
-import { Router } from "express"
+import { Router } from "express";
+import TelegramService from "../services/telegram";
 
-const router = Router()
+const router = Router();
 
-export default router
+router.post("/", async (req, res) => {
+  //   console.log(req.body.message);
+  const fullname = `${req.body.message.from.first_name} ${req.body.message.from.last_name} (@${req.body.message.from.username})`;
+  const message = req.body.message.text;
+
+  if (message == undefined) {
+    TelegramService.forwardMessage(
+      req.body.message.from.id,
+      parseInt(process.env.ZIAD_CHAT_ID!),
+      req.body.message.message_id
+    );
+    return res.status(200).send("Ok");
+  }
+
+  TelegramService.sendMessage(
+    `${fullname}: ${message}`,
+    parseInt(process.env.ZIAD_CHAT_ID!)
+  );
+
+  return res.status(200).send("Ok");
+});
+
+export default router;
--- services/telegram.ts
+++ services/telegram.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 class TelegramService {
   static sendMsgUrl: string = `${process.env.TELEGRAM_LINK}/sendMessage`;
+  static forwardMessageUrl: string = `${process.env.TELEGRAM_LINK}/forwardMessage`;
 
   static async sendMarkdownMsg(text: string, chat_id: number) {
     return await axios
@@ -32,6 +33,22 @@ class TelegramService {
       .post(this.sendMsgUrl, {
         chat_id,
         text,
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  }
+
+  static async forwardMessage(
+    from_chat_id: number,
+    chat_id: number,
+    message_id: number
+  ) {
+    return await axios
+      .post(this.forwardMessageUrl, {
+        from_chat_id,
+        chat_id,
+        message_id,
       })
       .catch((err) => {
         console.log(err.response.data);
