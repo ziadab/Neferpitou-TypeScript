a467806472fd406ba34b886ebe5fd21d73fd3856
Katarina <zain.fake02@gmail.com>
2023-05-24 19:47:58 +0100

    f

--- routes/compliment.ts
+++ routes/compliment.ts
@@ -23,6 +23,7 @@ router.get("/", async (req, res) => {
     { chat_id: 1862064937, name: "Knour" },
     { chat_id: 5755632631, name: "Mimi the cat" },
     { chat_id: 5626654638, name: "Lamyae" },
+    { chat_id: 6171371862, name: "Hibara" },
   ];
 
   users.map(async (user) => {
